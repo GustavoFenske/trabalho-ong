697049f68787eff1e6f24b158949030fcd1720a8
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-04-08 09:55:43 -0300

    add - adicionei algumas condições do js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,29 @@
 document.getElementById("formAdoção").addEventListener("submit", function (e) {
 
-                 e.preventDefault();
+    e.preventDefault();
 
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let cpf = document.getElementById("cpf").value
+    let idade = document.getElementById("idade").value
     let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
+
     if (nome.length < 3) return alert("Nome Inválido");
-    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+
+    if (email.includes("@")) {
+
+    } else {
+        return alert("o email precisa ter @");
+    }
+
+    if (telefone.length < 8) return alert("telefone invalido");
+
+    if (cpf.length <= 0) return alert("digite o seu cpf")
+
+    if ()
+        document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
 
 
 });
