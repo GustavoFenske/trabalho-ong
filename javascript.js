636486ce2f0ec6ae7076a670c7267a5ed8f07471
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-04-10 12:22:49 -0300

    add - adicionei mais regras

--- javascript.js
+++ javascript.js
@@ -7,8 +7,15 @@ document.getElementById("formAdoção").addEventListener("submit", function (e) 
     let telefone = document.getElementById("telefone").value;
     let cpf = document.getElementById("cpf").value
     let idade = document.getElementById("idade").value
+    let cidade = document.getElementById("cidade").value
     let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
+    let pets = document.querySelector('input[name="pets"]:checked');
+    let horas = document.getElementById("horas").value;
+    let motivo = document.getElementById("horas").value;
+
+
+
 
     if (nome.length < 3) return alert("Nome Inválido");
 
@@ -20,10 +27,21 @@ document.getElementById("formAdoção").addEventListener("submit", function (e) 
 
     if (telefone.length < 8) return alert("telefone invalido");
 
-    if (cpf.length <= 0) return alert("digite o seu cpf")
+    if (cpf.length <= 0) return alert("digite o seu cpf");
 
-    if ()
-        document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+    if (idade < 18) return alert("é necessário ser maior de idade");
+
+    if (cidade.length <= 0) return alert("digite a sua cidade");
+
+    if (moradia == false) return alert("escolha sua moradiaaz");
+
+    if (!quintal) return alert("assinale a opção do quintal");
+
+    if (!pets) return alert("assinale a opção do pets");
+
+    if (horas < 0 || horas > 24) return alert ("digite horas validas");
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
 
 
 });
